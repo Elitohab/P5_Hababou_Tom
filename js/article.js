a45f23f096e47f118672b231964c7c541a0f136e
Elito <tom.hababou@hotmail.fr>
2021-06-24 09:04:55 +0200

    html ok +article.js

--- js/article.js
+++ js/article.js
@@ -1,10 +1,25 @@
 const UrlApi = "http://localhost:3000/api/teddies";
+let param = (new URL(document.location)).searchParams;
+let id = param.get('id')
+
+console.log(id)
+
+async function getProduct(id) {
+    let response = await fetch(`${UrlApi}/${id}`)
+    let data = await response.json()
+   console.log(data)   
+    return data 
+  
+}
+
+getProduct(id)
+
 //Récupération des données dans l'API
-async function recupereDonneesApi () {
+/* async function recupereDonneesApi () {
     await fetch(UrlApi)
     .then((response) =>
         response.json()
     .then(data => 
         afficherArticles(data))
     ).catch(err => console.log('Erreur : ' + err));
-}
+}*/

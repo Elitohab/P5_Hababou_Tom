8e5599f11e0dc0cbd217969a185821681c2b8a9d
Elito <tom.hababou@hotmail.fr>
2021-07-01 09:04:41 +0200

    avancement js

---
+++ js/fonction.js
@@ -0,0 +1,23 @@
+// Instanciation de la variable qui stocke le contenu du panier
+const panierstorage = JSON.parse(localStorage.getItem('panier'));
+let panier = []
+let nbpanier = 0
+
+// Si le panier en local storage n'existe pas ou est vide
+if (panierstorage === null || panierstorage.length == 0) {
+    // Le panier est vide
+    panier = [];
+    // Le nombre d'articles dans le panier est de 0
+    nbPanier = 0;
+    // Si le panier en local storage exite et contient quelque chose
+} else {
+    // Le panier contient tous les articles dans le local storage 
+    panier = panierstorage;
+    // Le nombre d'articles dans le panier est le même que dans le local storage
+
+    nbPanier = panier.length
+    // On affiche en flex l'indication du nombre d'articles dans le panier
+    document.getElementById("nbPanier").style.display = "flex";
+    // On y insère le nombre d'articles présents dans le panier
+    document.getElementById("nbPanier").textContent = nbPanier;
+}
--- js/index.js
+++ js/index.js
@@ -1,29 +1,57 @@
-async function test() {
-    let produits = await getProduct()
-    afficheProduits(produits)
-}
-async function getProduct() {
-    let response = await fetch("http://localhost:3000/api/teddies")
+// On ,et dqns une vqriqble l'qdresse qui nous per;et de r2cup2rer tous les produits
+
+const urlAPIget = 'http://localhost:3000/api/teddies';
+
+/* Fonction qui s'éxécute toute seule au chargement de la page */ 
+
+(async function () {
+    // On affecte à la variable produits, la valeur retournée par la fonction getProduits
+    // Le await permet d'attendre que les produits soient récupérés pour passer à la ligne suivante
+    const produits = await getProduits() 
+    // Cette fonction permet d'afficher les produits qu'on lui passe en paramètre   
+    afficheProduits(produits) 
+})()
+
+/* Fonction de récupération des produits */
+
+async function getProduits() {
+    // On affecte à la variable response, ce que retourne le serveur quand on lui demande la liste des produits
+    // Le await permet d'attendre que le serveur nous ait répondu pour passer à la ligne suivante
+    let response = await fetch(urlAPIget)
+    // On affecte à la variable data la réponse formatée en json, ce qui permet de l'exploiter par la suite
+    // Le await permet d'attendre que le serveur nous ait répondu pour passer à la ligne suivante
     let data = await response.json()
+    // En cas d'erreur , on alerte
+    .catch((error) => {
+        alert("Connexion au serveur impossible")
+    })
+    // On retourne les données formatées
     return data 
 }
+
+/* Fonction d'affichage des produits */
+
 function afficheProduits(produits) {
-   let divProduits = document.getElementById("listNounours")
-   produits.forEach((produit) => {
-    let vignette = 
-    `<div class="col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 mt-4">
-        <a href="article.html?id=${produit._id}">
-            <div class="card carte">
-                <img id="product_img" class="card-img-top p-1" src="${produit.imageUrl}" alt="Appareil photo ${produit.name}">
-                <div class="card-body text-center">
-                    <h2 class="card-title h4">${produit.name}</h2>
-                    <p id="product_price" class="card-subtitle">${produit.price/100}.00 €</p>
-                    <button class="btn btn-info mx-auto mt-4 bouton-voir-produit">Voir le produit</button>
+    // On cible la div dans laquelle on veut afficher les produits, et on l'affecte à la variable divProduits
+    let divProduits = document.getElementById("listNounours")
+    // On boucle sur chaque produit trouvé dans le tableau produits
+    produits.forEach((produit) => {
+        // On instancie une variable qui contient tout le code html que l'on veut insérer dans la page HTML
+        let vignette = 
+        `<div class="col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 mt-4">
+            <a href="article.html?id=${produit._id}">
+                <div class="card carte">
+                    <img class="card-img-top p-1 carte__image" src="${produit.imageUrl}" alt="Nounours ${produit.name}">
+                    <div class="card-body text-center">
+                        <h2 class="card-title h4">${produit.name}</h2>
+                        <p class="card-subtitle">${produit.price/100}.00 €</p>
+                        <button class="btn btn-info mx-auto mt-4 bouton-voir-produit">Voir le produit</button>
+                    </div>
                 </div>
-            </div>
-        </a>
-    </div>`;
-    divProduits.innerHTML+= vignette
+            </a>
+        </div>`;
+        // On insère le contenu de vignette à l'emplacement prévu dans la page HTML
+        // Le += permet d'ajouter du contenu à divProduits sans effacer le précédent
+        divProduits.innerHTML+= vignette
    })
 }
-test()
